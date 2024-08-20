2660be902ee3437e7bf52c4f6ea0e02466252abd
Yunxia Zhang <onvanessano@gmail.com>
2024-08-20 01:10:31 -0700

    fixing bug: forget to add await in front of the database function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,4 @@
 import express from "express";
-// import asyncHandler from "../middleware/asyncHandler.js";
-// import Product from "../models/productModel.js";
 import {
   getProducts,
   getProductById,
@@ -13,26 +11,5 @@ const router = express.Router();
 router.route("/").get(getProducts);
 // Get product by Id
 router.route("/:id").get(getProductById);
-
-// router.get(
-//   "/",
-//   asyncHandler(async (req, res) => {
-//     const products = await Product.find({});
-//     res.json(products);
-//   })
-// );
-
-// router.get(
-//   "/:id",
-//   asyncHandler(async (req, res) => {
-//     const product = await Product.findById(req.params.id);
-
-//     if (product) {
-//       return res.json(product);
-//     }
-//     res.status(404);
-//     throw new Error("Resource not found"); // More decent errorHandler
-//   })
-// );
 
 export default router;
