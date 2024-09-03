03345452d7a85d17a8de305e223ebe4d10823d1d
Yunxia Zhang <onvanessano@gmail.com>
2024-09-03 15:37:20 -0700

    Create protect and admin middleware to use jwt httponly cookie secure the user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,18 +10,26 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddlewre.js";
 
 const router = express.Router();
 
 // router.route('endpoint').method(controller)
 // Get all the users by Admin, post registeruser
-router.route("/").get(getUsers).post(registerUser);
+router.route("/").get(protect, admin, getUsers).post(registerUser);
 // Post login and logout
 router.post("/logout", logoutUser);
-router.post("/login", authUser);
+router.post("/auth", authUser);
 // Get user profile, update user profile
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile); // Add the protect middleware in front of the get and put route
 // Manipulate User by Id
-router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
 
 export default router;
