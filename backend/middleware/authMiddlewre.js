27fab739cb6e5579c99d62ea0cd6543c4faf1629
Yunxia Zhang <onvanessano@gmail.com>
2024-09-03 13:50:38 -0700

    Created authMiddleware from jwt http only cookie, use protect and admin function to control the userRoutes

---
+++ backend/middleware/authMiddlewre.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler";
+import User from "../models/userModel";
+
+// Protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Read the JWT from the cookie, in the usercontroller the token's name was set as jwt
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Get the info in the payload
+      req.user = await User.findById(decoded.userId).select("-password"); // Don't want the password to be fetched, only put the user into the req
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+// Admin middlerware
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authrized as admin");
+  }
+};
+
+export { protect, admin };
