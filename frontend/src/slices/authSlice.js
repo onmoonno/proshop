eac75812fb96a6308acf4c930c417ede7138ed43
Yunxia Zhang <onvanessano@gmail.com>
2024-09-08 13:24:09 -0700

    Add loggout reducer to the authSlice to destroy cookie and clear userInfo in localstorage

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -14,6 +14,10 @@ const authSlice = createSlice({
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
+    loggout: (state) => {
+      state.userInfo = null;
+      localStorage.removeItem("userInfo");
+    },
   },
 });
 
